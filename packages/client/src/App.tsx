ac5cbc321359bb35c3d7606242f3f013223aa180
Alexandros Georgantas <alexgeorg86@gmail.com>
2024-06-16 11:32:09 +0300

    init base client

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { App, Button, Space, Layout, Menu, theme } from 'antd'
+const { Header, Footer, Content } = Layout
+const layoutStyle = {
+  overflow: 'hidden',
+  width: '100%',
+  height: '100%',
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+const MyPage = () => {
+  const { message, modal, notification } = App.useApp()
+
+  const showMessage = () => {
+    message.success('Success!')
+  }
+
+  const showModal = () => {
+    modal.warning({
+      title: 'This is a warning message',
+      content: 'some messages...some messages...',
+    })
+  }
+
+  const showNotification = () => {
+    notification.info({
+      message: `Notification topLeft`,
+      description: 'Hello, Ant Design!!',
+      placement: 'topLeft',
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Space wrap>
+      <Button type="primary" onClick={showMessage}>
+        Open message
+      </Button>
+      <Button type="primary" onClick={showModal}>
+        Open modal
+      </Button>
+      <Button type="primary" onClick={showNotification}>
+        Open notification
+      </Button>
+    </Space>
+  )
+}
+const items = new Array(3).fill(null).map((_, index) => ({
+  key: String(index + 1),
+  label: `nav ${index + 1}`,
+}))
+const MyApp: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken()
+
+  return (
+    <App style={{ height: '100vh' }}>
+      <Layout style={layoutStyle}>
+        <Header style={{ display: 'flex', alignItems: 'center' }}>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            items={items}
+            style={{ flex: 1, minWidth: 0 }}
+          />
+        </Header>
+        <Content style={{ padding: '0 48px', height: 'calc(100vh - 64px)' }}>
+          <div
+            style={{
+              background: colorBgContainer,
+              minHeight: 280,
+              padding: 24,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <MyPage />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          GWI ©{new Date().getFullYear()} Created by Alexandros Georgantas
+        </Footer>
+      </Layout>
+    </App>
   )
 }
 
-export default App
+export default MyApp
